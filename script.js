0f719b89c827f7b78290f4e8d46ba9c694514b85
Marat Sayetgarayev <stgrmar@gmail.com>
2023-01-20 21:27:35 +0300

    сделал второй дз

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = 'Калькулятор';
+let screens = 'Простые, Сложные, интерактивные';
+let screenPrice = 100;
+let rollback = 90;
+let fullPrice = 500;
+let adaptive = true;
 
-alert('Внимание! Спасибо за внимание!');
-console.log('И тут меня нашли')
+// alert('Внимание! Спасибо за внимание!');
+// console.log('И тут меня нашли')
+
+console.log(typeof title);
+
+console.log(typeof fullPrice);
+
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log(screenPrice);
+
+console.log(screenPrice + ' рублей');
+
+console.log(fullPrice + ' рублей');
+
+console.log(screens.toLowerCase());
+
+console.log(screens.split(', '));
+
+console.log(fullPrice*(rollback/100))
