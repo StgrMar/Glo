9d49697b0ee890c6b726c64073851b960e3fbf1c
Marat Sayetgarayev <stgrmar@gmail.com>
2023-01-23 21:57:29 +0300

    сделал дз 3

--- script.js
+++ script.js
@@ -4,26 +4,49 @@ let screenPrice = 100;
 let rollback = 90;
 let fullPrice = 500;
 let adaptive = true;
+let service1 = 'any';
+let service2 = 'many';
+let servicePrice1 = 100;
+let servicePrice2 = 100;
+let servicePercentPrice;
+let percent;
 
-// alert('Внимание! Спасибо за внимание!');
-// console.log('И тут меня нашли')
+title = prompt(' как называется ваш проект?', 'Калькулятор');
+screens = prompt(' Какик типы экранов нужно разработать?', 'сложные');
+screenPrice = +prompt(' Сколько будет стоить данная работа?');
+adaptive = confirm(' Нужен ли адаптив на сайте?');
+service1 = prompt(' Какой дополнительный тип услуги нужен?', 'развертка');
+servicePrice1 = +prompt(' Сколько будет стоить данная работа?');
+service2 = prompt(' Какой дополнительный тип услуги нужен?', 'отвертка');
+servicePrice2 = +prompt(' Сколько будет стоить данная работа?');
+fullPrice = screenPrice + servicePrice1 + servicePrice2;
 
-console.log(typeof title);
+if (fullPrice > 30000) {
+    console.log(' даем скидку в 10%');
+    percent = 0.1 * fullPrice; 
+} else if (fullPrice >= 15000 && fullPrice <= 30000) {
+    console.log(' даем скидку в 5%');
+    percent = 0.05 * fullPrice;
+} else if (fullPrice > 0 && fullPrice < 15000) {
+    console.log( ' скидка не предусмотрена');
+    percent = 0 * fullPrice;
+} else {
+    console.log(' Что-то пошло не так :(')
+    };
 
-console.log(typeof fullPrice);
+servicePercentPrice = fullPrice - percent;
 
-console.log(typeof adaptive);
+    console.log('Название проекта : ' + title);
+    console.log('Типы экранов : ' + screens);
+    console.log('Цена за работу : ' + screenPrice);
+    console.log('Нужен ли адаптив : ' + adaptive);
+    console.log('Услуга1 : ' + service1);
+    console.log('Услуга1 цена : ' + servicePrice1);
+    console.log('Услуга2 : ' + service2);
+    console.log('Услуга2 цена : ' + servicePrice2);
+    console.log('Цена общая : ' + fullPrice);
+    console.log('Итоговая стоимость : ' + Math.ceil(servicePercentPrice));
 
-console.log(screens.length);
+    alert('Итоговая цена : ' + servicePercentPrice + '. ');
 
-console.log(screenPrice);
 
-console.log(screenPrice + ' рублей');
-
-console.log(fullPrice + ' рублей');
-
-console.log(screens.toLowerCase());
-
-console.log(screens.split(', '));
-
-console.log(fullPrice*(rollback/100))
