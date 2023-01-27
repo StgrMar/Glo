c91344af0925675748d79c86b1ae372316a742e2
Marat Sayetgarayev <stgrmar@gmail.com>
2023-01-27 22:20:26 +0300

    сделал 4е дз

--- script.js
+++ script.js
@@ -1,42 +1,70 @@
-let title = 'Калькулятор';
-let screens = 'Простые, Сложные, интерактивные';
-let screenPrice = 100;
-let rollback = 90;
-let fullPrice = 500;
-let adaptive = true;
-let service1 = 'any';
-let service2 = 'many';
-let servicePrice1 = 100;
-let servicePrice2 = 100;
+'use strict'
+let title = prompt(' как называется ваш проект?', '  кАлькУлЯтор Верстки');
+let screens = prompt(' Какик типы экранов нужно разработать?', 'сложные');
+let screenPrice = +prompt(' Сколько будет стоить данная работа?', 25000);
+let rollback = 10;
+let adaptive = confirm(' Нужен ли адаптив на сайте?');
+let service1 = prompt(' Какой дополнительный тип услуги нужен?', 'развертка');
+let servicePrice1 = +prompt(' Сколько будет стоить данная работа?', 2500);
+let service2 = prompt(' Какой дополнительный тип услуги нужен?', 'отвертка');
+let servicePrice2 = +prompt(' Сколько будет стоить данная работа?', 3500);
+// let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 let servicePercentPrice;
 let percent;
 
-title = prompt(' как называется ваш проект?', 'Калькулятор');
-screens = prompt(' Какик типы экранов нужно разработать?', 'сложные');
-screenPrice = +prompt(' Сколько будет стоить данная работа?');
-adaptive = confirm(' Нужен ли адаптив на сайте?');
-service1 = prompt(' Какой дополнительный тип услуги нужен?', 'развертка');
-servicePrice1 = +prompt(' Сколько будет стоить данная работа?');
-service2 = prompt(' Какой дополнительный тип услуги нужен?', 'отвертка');
-servicePrice2 = +prompt(' Сколько будет стоить данная работа?');
-fullPrice = screenPrice + servicePrice1 + servicePrice2;
+const showTypeOf = function (variable) {
+    console.log(variable, typeof variable);
+};
 
-if (fullPrice > 30000) {
-    console.log(' даем скидку в 10%');
-    percent = 0.1 * fullPrice; 
-} else if (fullPrice >= 15000 && fullPrice <= 30000) {
-    console.log(' даем скидку в 5%');
-    percent = 0.05 * fullPrice;
-} else if (fullPrice > 0 && fullPrice < 15000) {
-    console.log( ' скидка не предусмотрена');
-    percent = 0 * fullPrice;
-} else {
-    console.log(' Что-то пошло не так :(')
-    };
+// тип function-expression
+const getAllServicePrices = function (sP1, sP2) {
+    return sP1 + sP2;
+};
 
-servicePercentPrice = fullPrice - percent;
+const allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
 
-    console.log('Название проекта : ' + title);
+// тип function-declaration
+function getFullPrice(screenPrice, allServicePrices) {
+    return screenPrice + allServicePrices;
+};
+
+const fullPrice = getFullPrice(screenPrice, allServicePrices);
+
+// Объявить функцию getTitle. Функция возвращает title меняя его таким образом: 
+// первый символ с большой буквы, остальные с маленькой". Учесть вариант что строка может начинаться с пустых символов." КаЛьКулятор Верстки"
+const getTitle = function(title) {
+    return title.trim()[0].toUpperCase() + title.trim().substr(1).toLowerCase();
+};
+//trim() позволяет удалить пробелы с обоих концов строки, затем Нулевой индекс переводит на высокий регистр благодяра toUpperCase() 
+//substr() позволяет извлечь из строки определенное количество символов, начиная с заданного индекса. Получается, что начиная
+//с 1го индекса все остальные символы будут прописываться с низким регистром. Второй trim() нужен чтобы тоже удалить все пробелы
+// первый трим для второго слагаемого не действует
+
+const getRollbackMessage = function(price) {
+    if (price > 30000) {
+        percent = 0.1 * price;
+        return 'даем скидку в 10%';
+    } else if (price >= 15000 && price <= 30000) {
+        percent = 0.05 * price;
+        return 'даем скидку в 5%';
+    } else if (price > 0 && price < 15000) {
+        percent = 0 * price;
+        return ' скидка не предусмотрена';
+    } else {
+        return ' Что-то пошло не так :(';
+        };
+    
+};
+
+const getServicePercentPrices = function(servicePercentPrice) {
+    return fullPrice - percent;
+};
+
+showTypeOf(title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
+    console.log(getRollbackMessage(fullPrice));
+    console.log('Название проекта : ' + getTitle(title));
     console.log('Типы экранов : ' + screens);
     console.log('Цена за работу : ' + screenPrice);
     console.log('Нужен ли адаптив : ' + adaptive);
@@ -45,8 +73,8 @@ servicePercentPrice = fullPrice - percent;
     console.log('Услуга2 : ' + service2);
     console.log('Услуга2 цена : ' + servicePrice2);
     console.log('Цена общая : ' + fullPrice);
-    console.log('Итоговая стоимость : ' + Math.ceil(servicePercentPrice));
+    console.log('Итоговая стоимость : ' + Math.ceil(getServicePercentPrices(servicePercentPrice)));
 
-    alert('Итоговая цена : ' + servicePercentPrice + '. ');
+    alert('Итоговая цена : ' + Math.ceil(getServicePercentPrices(servicePercentPrice)) + '. ');
 
 
